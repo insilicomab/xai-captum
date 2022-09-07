0fae52b959c3ae0b649cbc35d8d9e1c00ed27d38
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2022-09-07 14:42:20 +0900

    Update dataloader.py

--- scripts/utils/dataloader.py
+++ scripts/utils/dataloader.py
@@ -7,19 +7,9 @@ from torchvision import transforms
 # class to load input images and processed original images
 class ImageLoader():
     
-    def __init__(self, img_dir: str, photo_size: int):
+    def __init__(self, img_dir: str, transform=None):
         self.img_dir = img_dir
-        self.photo_size = photo_size
-        self.transform = {
-            'input': transforms.Compose([
-                transforms.Resize([self.photo_size, self.photo_size]),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-                ]),
-            'original': transforms.Compose([
-                transforms.Resize([self.photo_size, self.photo_size]),
-                ]),
-        }
+        self.transform = transform
 
     
     def process_images(self):
@@ -28,9 +18,9 @@ class ImageLoader():
         img = Image.open(self.img_dir).convert('RGB')
 
         # process input image
-        input_img = self.transform['input'](img).unsqueeze(0)
+        input_img = self.transform('input', img).unsqueeze(0)
 
         # process original image
-        original_img = np.array(self.transform['original'](img))
+        original_img = np.array(self.transform('original',img))
 
         return input_img, original_img
